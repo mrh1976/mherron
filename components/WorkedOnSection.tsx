25e32995b5034ac28885f7a842e56c449a5506fc
mrh1976 <mherron54@gmail.com>
2026-04-10 19:18:50 -0500

    Update WorkedOnSection.tsx

--- components/WorkedOnSection.tsx
+++ components/WorkedOnSection.tsx
@@ -109,7 +109,7 @@ export default function WorkedOnSection() {
                           {imageUrl ? (
                             <Image
                               src={imageUrl}
-                              alt={project.title}
+                              alt={`${project.title} - ${project.company} marketing campaign by Michael Herron`}
                               fill
                               className="object-cover transition-transform duration-300 group-hover:scale-105"
                               sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
